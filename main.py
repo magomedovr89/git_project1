029c1ff11b3c8e814ba44ec0d324ef76aca4051d
Smart developer <magomedovr89@gmail.com>
2022-11-26 15:59:13 +0300

    Second commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def print_hi():
     # Use a breakpoint in the code line below to debug your script.
     print(f'My first git program')  # Press Ctrl+F8 to toggle the breakpoint.
     print('Наш первый коммит')
+    print('Наш второй коммит который позволит нам посмотреть изменения в файле')
 
 
 # Press the green button in the gutter to run the script.
