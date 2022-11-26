1cf6d59c150588ad6de0f7674bd9c8df40380a97
Smart developer <magomedovr89@gmail.com>
2022-11-26 15:55:35 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def print_hi():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'My first git program')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Наш первый коммит')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
